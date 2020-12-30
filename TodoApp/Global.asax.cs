8b0782d2e68dc8630b111e48c0dc16c0f9de870a
toshinori1212 <u36c4zed72vx1@gmail.com>
2020-12-30 13:12:28 +0900

    first commit

---
+++ TodoApp/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using TodoApp.Migrations;
+using TodoApp.Models;
+
+namespace TodoApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()//アプリを起動したときに最初に呼ばれるメソッド
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TodoesContext, Configration>());
+        }
+    }
+}
---
+++ TodoApp/Migrations/Configration.cs
@@ -0,0 +1,58 @@
+﻿namespace TodoApp.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using TodoApp.Models;
+
+    internal sealed class Configration : DbMigrationsConfiguration<TodoApp.Models.TodoesContext>
+    {
+        public Configration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;//データが削除される変更を許可するか
+            ContextKey = "TodoApp.Models.TodoesContext";
+        }
+
+        protected override void Seed(TodoApp.Models.TodoesContext context)//マイグレーションされたときに自動で実行される。
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+            User admin = new User()
+            {
+                Id = 1,
+                UserName = "admin",
+                Password = "password",
+                Roles = new List<Role>()
+            };
+
+            Role administrators = new Role()
+            {
+                Id = 1,
+                RoleName = "Administrators",
+                Users = new List<User>()
+            };
+
+            Role users = new Role()
+            {
+                Id = 2,
+                RoleName = "Users",
+                Users = new List<User>()
+            };
+
+            var membershipProbider = new CustomMembershipProvider();
+
+            admin.Password = membershipProbider.GeneratePasswordHash(admin.UserName, admin.Password);
+            admin.Roles.Add(administrators);
+            administrators.Users.Add(admin);
+
+            context.Roles.AddOrUpdate(role => role.Id, new Role[] { administrators, users });
+
+        }
+    }
+}
